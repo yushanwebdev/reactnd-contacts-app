1185b2ce75d4137819c409740bc760326ac5eb53
yushan <yushan@intervest.lk>
2020-12-27 00:15:32 +0530

    feat: Add the contacts prop to ContactList component

--- src/App.js
+++ src/App.js
@@ -4,9 +4,30 @@ import ContactList from './ContactList';
 
 class App extends Component {
   render() {
+    const contacts = [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ];
+
     return (
       <div>
-        <ContactList />
+        <ContactList contacts={contacts} />
       </div>
     );
   }
