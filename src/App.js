1cd1d7eee9ff060da49190406b11357323ee2b65
yushan <yushan@intervest.lk>
2020-12-26 12:27:15 +0530

    feat: Ceate the ContactList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import ContactList from './ContactList';
+
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <ContactList />
       </div>
     );
   }
---
+++ src/ContactList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ContactList extends Component {
+    render() {
+        return(
+            <ol className='contact-list'>
+
+            </ol>
+        );
+    }
+}
+
+export default ContactList;
