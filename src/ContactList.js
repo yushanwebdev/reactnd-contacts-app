3940d217b3940883aa285dc4cbbd91ab176bb6cc
yushan <yushan@intervest.lk>
2020-12-27 00:22:34 +0530

    feat: ContactList change to a functional component

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ContactList extends Component {
-    render() {
-        return(
-            <ol className='contact-list'>
-
-            </ol>
-        );
-    }
+function ContactList(props) {
+    return (
+        <ol className='contact-list'>
+            {
+                props.contacts.map((contact) => (
+                    <li key={contact.id}>
+                        <div
+                            className='contact-avatar'
+                            style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }}>
+                        </div>
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>{contact.handle}</p>
+                        </div>
+                        <button className='contact-remove'>
+                            Remove
+                    </button>
+                    </li>
+                ))
+            }
+        </ol>
+    )
 }
 
 export default ContactList;
