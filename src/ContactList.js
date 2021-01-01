06d4480eae0e5475975af3bb2872685108359bf9
yushan <yushan@intervest.lk>
2021-01-01 21:09:27 +0530

    feat: Use PropTypes for type checking the ContactList props

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function ContactList(props) {
     return (
@@ -24,6 +25,11 @@ function ContactList(props) {
             }
         </ol>
     )
+}
+
+ContactList.propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
 }
 
 export default ContactList;
