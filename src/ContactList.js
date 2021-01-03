cc6780e2eb2b42d6cd7f27727bbd8523b698b873
yushan <yushan@intervest.lk>
2021-01-03 08:35:25 +0530

    feat: Add the search input field

--- src/ContactList.js
+++ src/ContactList.js
@@ -7,29 +7,52 @@ class ContactList extends Component {
         onDeleteContact: PropTypes.func.isRequired
     }
 
+    state = {
+        query: ''
+    }
+
+    updateQuery = query => {
+        this.setState(() => ({
+            query: query.trim()
+        }))
+    }
+
     render() {
         return (
-            <ol className='contact-list'>
-                {
-                    this.props.contacts.map((contact) => (
-                        <li key={contact.id}>
-                            <div
-                                className='contact-avatar'
-                                style={{
-                                    backgroundImage: `url(${contact.avatarURL})`
-                                }}>
-                            </div>
-                            <div className='contact-details'>
-                                <p>{contact.name}</p>
-                                <p>{contact.handle}</p>
-                            </div>
-                            <button className='contact-remove' onClick={() => this.props.onDeleteContact(contact)}>
-                                Remove
-                            </button>
-                        </li>
-                    ))
-                }
-            </ol>
+            <div className='list-contacts'>
+                {/* this line uses for debugging purposes */}
+                {JSON.stringify(this.state)}
+                <div className='list-contacts-top'>
+                    <input
+                        className='search-contacts'
+                        type='text'
+                        placeholder='Search Contacts'
+                        value={this.state.query}
+                        onChange={(event) => this.updateQuery(event.target.value)}
+                    />
+                </div>
+                <ol className='contact-list'>
+                    {
+                        this.props.contacts.map((contact) => (
+                            <li key={contact.id}>
+                                <div
+                                    className='contact-avatar'
+                                    style={{
+                                        backgroundImage: `url(${contact.avatarURL})`
+                                    }}>
+                                </div>
+                                <div className='contact-details'>
+                                    <p>{contact.name}</p>
+                                    <p>{contact.handle}</p>
+                                </div>
+                                <button className='contact-remove' onClick={() => this.props.onDeleteContact(contact)}>
+                                    Remove
+                                </button>
+                            </li>
+                        ))
+                    }
+                </ol>
+            </div>
         )
     }
 }
