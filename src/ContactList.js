299b215614ce90159e7a9e7626cd39a6a19ff0ee
yushan <yushan@intervest.lk>
2021-01-03 08:17:08 +0530

    refactor: convert ContactList to a Class component

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,35 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-function ContactList(props) {
-    return (
-        <ol className='contact-list'>
-            {
-                props.contacts.map((contact) => (
-                    <li key={contact.id}>
-                        <div
-                            className='contact-avatar'
-                            style={{
-                                backgroundImage: `url(${contact.avatarURL})`
-                            }}>
-                        </div>
-                        <div className='contact-details'>
-                            <p>{contact.name}</p>
-                            <p>{contact.handle}</p>
-                        </div>
-                        <button className='contact-remove' onClick={() => props.onDeleteContact(contact)}>
-                            Remove
+class ContactList extends Component {
+    render() {
+        return (
+            <ol className='contact-list'>
+                {
+                    props.contacts.map((contact) => (
+                        <li key={contact.id}>
+                            <div
+                                className='contact-avatar'
+                                style={{
+                                    backgroundImage: `url(${contact.avatarURL})`
+                                }}>
+                            </div>
+                            <div className='contact-details'>
+                                <p>{contact.name}</p>
+                                <p>{contact.handle}</p>
+                            </div>
+                            <button className='contact-remove' onClick={() => props.onDeleteContact(contact)}>
+                                Remove
                     </button>
-                    </li>
-                ))
-            }
-        </ol>
-    )
-}
-
-ContactList.propTypes = {
-    contacts: PropTypes.array.isRequired,
-    onDeleteContact: PropTypes.func.isRequired
+                        </li>
+                    ))
+                }
+            </ol>
+        )
+    }
 }
 
 export default ContactList;
