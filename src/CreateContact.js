17e02aa29389d64ee0649abc9e05e2350c9b6c47
yushan <yushan@intervest.lk>
2021-01-28 22:04:48 +0530

    feat: Get the form data into the submit handler

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ImageInput from './ImageInput';
+import serializeForm from 'form-serialize';
 
 class CreateContact extends Component {
+    handleSubmit = e => {
+        e.preventDefault();
+        const values = serializeForm(e.target, { hash: true });
+        
+        console.log(values);
+    }
+
     render() {
         return (
             <div>
@@ -11,7 +19,7 @@ class CreateContact extends Component {
                     to='/'>
                     Close
                 </Link>
-                <form className='create-contact-form'>
+                <form onSubmit={this.handleSubmit} className='create-contact-form'>
                     <ImageInput
                         className='create-contact-avatar-input'
                         name='avatarURL'
